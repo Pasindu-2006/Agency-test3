ed6d469f093e80582f7edc5ef683b088f57a65f7
Pasindu manodya <pasindumanodya2@gmail.com>
2026-09-06 12:38:34 +0530

    Edited code

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -30,23 +30,25 @@ const Services = () => {
     ]
 
   return (
-    <motion.div
-      initial="hidden"
-      whileInView="visible"
-      transition={{staggerChildren:0.2}}
-      viewport={{once:true}}
+    <div
 
     id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
 
         <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
         
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 mt-5'>
+      <motion.div
+      initial="hidden"
+      whileInView="visible"
+      transition={{staggerChildren:0.2}}
+      viewport={{once:true}} 
+      
+      className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 mt-5'>
         {servicesData.map((service, index)=>(
           <ServiceCard key={index} service={service} index={index}/>
         ))}
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   )
 }
 
